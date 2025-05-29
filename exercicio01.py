4759c07cb483753118c30ce4d54865618f083bf8
043ianzinn <ian.tonete@escola.pr.gov.br>
2025-05-29 14:37:16 +0000

    metade exercicio01

--- exercicio01.py
+++ exercicio01.py
@@ -2,5 +2,7 @@
 # Ele quer saber qual curso teve mais avaliações no último mês.
 
 # Crie um programa que receba o número de avaliações de cada curso e exiba qual teve mais.
-# Caso as quantidades sejam iguais, exiba uma mensagem dizendo que houve empate.
+# Caso as quantidades sejam iguais, exiba uma mensagem dizendo que houve 
 
+nome = "Ian Maecos"
+idade = 
